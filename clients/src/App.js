2831e1f2bd228612fb433aae26d4bcbb068081ae
Truong Nguyen <you@example.com>
2024-05-23 02:10:39 +0700

    update: redux slice and theme

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Auth from './pages/Auth/Auth';
+import Profile from './pages/Profile/Profile';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
